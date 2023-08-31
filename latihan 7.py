ece9643a76f17578f240b6abe1958d4454b24326
444jasmine <142990283+444jasmine@users.noreply.github.com>
2023-08-31 21:10:53 +0700

    Add files via upload

---
+++ latihan 7.py
@@ -0,0 +1,29 @@
+z =[1, 3, 2, 4, 'Alice', 'Bob']
+z.sort(key=str)
+print(z)
+print("Hello there!\nHow are you?\nI\'m doing fine.")
+multi_line = """Hello there!
+How are you?
+I'm fine."""
+print(multi_line)
+spam = ' Hello World '
+spam.strip()
+spam.lstrip()
+spam.rstrip()
+', '.join(['cats', 'rats', 'bats'])
+print(', '.join(['cats', 'rats', 'bats']))
+' '.join(['My', 'name', 'is', 'Simon'])
+print(' '.join(['My', 'name', 'is', 'Simon']))
+'ABC'.join(['My', 'name', 'is', 'Simon'])
+print('ABC'.join(['My', 'name', 'is', 'Simon']))
+'My name is Simon'.split()
+print('My name is Simon'.split())
+'MyABCnameABCisABCSimon'.split('ABC')
+print('MyABCnameABCisABCSimon'.split('ABC'))
+'My name is Simon'.split('m')
+print('My name is Simon'.split('m'))
+print(spam)
+
+
+
+
---
+++ latihan 9.py
@@ -0,0 +1,2 @@
+nim = "23/521423/SV/32428"
+print(nim.replace("/", ""))
